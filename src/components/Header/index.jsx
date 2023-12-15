2c60bef92754f41ffeb818e2d792eea6bf1f972c
LL <liaposhchenkol@dmail.com>
2023-12-15 21:55:53 +0100

    Update Header on the orderPage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import './index.css';
 
-export const Header = ({}) => {
+export const Header = ({ showMenu }) => {
   return (
     <header id="home">
       <div className="header__content container">
@@ -8,12 +8,18 @@ export const Header = ({}) => {
 
         <div className="navigation">
           <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
+          {showMenu ? (
+            <nav className="rollout-nav nav-closed">
+              <a href="/#home">domů</a>
+            </nav>
+          ) : (
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+              <a href="#menu">menu</a>
+              <a href="#gallery">galerie</a>
+              <a href="#contact">kontakt</a>
+            </nav>
+          )}
         </div>
       </div>
     </header>
